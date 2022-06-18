b4d3c6ffebf676a39c29bf0653bec127d7da4ba6
deni zakya <denizakya@gmail.com>
2022-06-18 08:31:24 +0200

    Add full path for output

--- Plugins/GitHistory/plugin.swift
+++ Plugins/GitHistory/plugin.swift
@@ -3,12 +3,13 @@ import PackagePlugin
 
 @main struct GitHistory: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        let output = context.pluginWorkDirectory.appending([".history"])
         return [
             .prebuildCommand(displayName: "Last Git commit",
                              executable: .init("/usr/bin/git"),
                              arguments: [
                                 "log", "--oneline", "--format=\"%s\"", "-n 5"],
-                             outputFilesDirectory: .init("~/.history"))
+                             outputFilesDirectory: output)
         ]
     }
 }
